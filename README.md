fec917cddd3cbb8777a2ca324f33213c0efcee01
yalooong <yalong.xyz@gmail.com>
2018-01-28 20:54:11 -0500

    init files

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# phonenumber-parser
-A simple RESTful api server
+# Phonenumber-parser
+
+A simple RESTful api server built for parsing phone numbers from text or files by using google libphonenumber library.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+Any operating systems with NodeJS installed and a working brawser.
+
+```
+NodeJS: https://nodejs.org/en/download/
+Chrome: https://www.google.com/chrome/browser/desktop/index.html
+```
+
+### Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+clone the repository to your local machine
+
+```
+git clone https://github.com/yalooong/phonenumber-parser.git
+```
+
+run the following command from phonenumber-parser root
+
+```
+npm install
+node server.js
+```
+
+The server should be started on localhost:3000.
+
+## Running the tests
+
+Two APIs are accessable at:
+
+GET /api/phonenumbers/parse/text/{...string...}
+This will send back an array of numbers if the string contains any.
+
+```
+http://localhost:3000/api/phonenumbers/parse/text/anytextandnumber4168391029
+```
+
+POST http://localhost:3000/api/phonenumbers/parse/file
+This allow you to upload a file, and parse the phone numbers within the file.
+
+```
+http://localhost:3000/api/phonenumbers/parse/file
+```
+
+## Built With
+
+* [Express](https://expressjs.com/) - The web framework used
+* [npm](https://maven.apache.org/) - Package Management for JavaScript
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ server.js
@@ -0,0 +1,80 @@
+// app.js
+
+// call packages
+var express = require('express');
+var bodyParser = require('body-parser');
+var upload = require('express-fileupload');
+var libphonenumber = require('libphonenumber-js');
+
+var app = express();
+var port = process.env.port || 3000;
+
+// Routes for API
+var router = express.Router();
+
+// test route
+router.get('/test', (req, res) => {
+    res.json({message: 'it works.'});
+});
+
+// GET /api/phonenumbers/parse/text/{...string...}
+// allow sending small snippets of text to be parsed.
+router.get('/text/:string', (req, res) => {
+    var rt = [];
+    var text = req.params.string;
+    var result = libphonenumber.parse(text, "CA");
+
+    if (!isEmpty(result)) {
+        rt.push(libphonenumber.format(result, 'National'));
+    }
+    return res.status(200).send(rt);
+});
+
+// send file page
+router.get('/file', (req, res) => {
+    res.sendFile(__dirname + "/fileupload.html");
+});
+
+// POST /api/phonenumbers/parse/file
+router.post('/file', function(req, res) {
+
+    // nothing uploaded.
+    if (!req.files) {
+        return res.status(400).send('No files were uploaded.');
+    } else {
+        let mfile = req.files.mfile;
+        var rt = [];
+        
+        var fs = require('fs');
+        fs.readFileSync(mfile.name).toString().split('\n').forEach(function (line) {
+            var phone = libphonenumber.parse(line, "CA");
+            
+            if (!isEmpty(phone)) {
+                rt.push(libphonenumber.format(phone, 'National'));
+            }
+        })
+
+        return res.status(200).send(rt);
+    }
+});
+
+// localhost status
+app.get('/', (req, res) => {
+    return res.status(200).send('API works.');
+});
+
+// Register
+app.use(upload());
+app.use('/api/phonenumbers/parse', router);
+
+// Start server
+app.listen(port);
+console.log('The server is on port ' + port);
+
+// util function to check if object is empty
+function isEmpty(obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
+}
