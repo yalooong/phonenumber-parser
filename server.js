b7ae84a43768cdde0020fe6047c9c45492cab0e9
mgquan@myseneca.ca <mgquan@myseneca.ca>
2018-02-09 16:02:41 -0500

    added url parsing support

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var upload = require('express-fileupload');
 var libphonenumber = require('libphonenumber-js');
+var https = require('https');
+var http = require('http');
 
 var app = express();
 var port = process.env.port || 3000;
@@ -30,6 +32,84 @@ router.get('/text/:string', (req, res) => {
     return res.status(200).send(rt);
 });
 
+// GET /api/phonenumbers/parse/url/{...website...}
+// allow sending of url links to be parsed.
+//note need  to send as www.{...string...}.com to work properly work
+router.get('/http/:string', (req, res) => {
+    var url = require('url');
+    var rt = [];
+    var text = req.params.string;
+    console.log(text);
+
+    var options = {
+      host: text,
+      path: '/',
+    }
+    //note the ress. had to put that because it conflicts with res in router
+    var request = http.request(options, function (ress) {
+      var data = '';
+      ress.on('data', function (chunk) {
+        data += chunk;
+      });
+      ress.on('end', function () {
+        var textVersion = require("textversionjs");
+        var plainText = textVersion(data);
+        var phrases = data.split("\n");
+        console.log(phrases);
+        for(var i = 0; i < phrases.length; i++){
+          var result = libphonenumber.parse(phrases[i], "CA");
+          if (!isEmpty(result)) {
+              rt.push(libphonenumber.format(result, 'National'));
+          }
+        }
+        return res.status(200).send(rt);
+      });
+    });
+    request.on('error', function (err) {
+      console.log(err.message);
+    });
+    request.end();
+});
+
+// GET /api/phonenumbers/parse/https/{...website...}
+// allow sending of url links to be parsed.
+// note need  to send as www.{...string...}.com to work properly work
+router.get('/https/:string', (req, res) => {
+    var url = require('url');
+    var rt = [];
+    var text = req.params.string;
+    console.log(text);
+
+    var options = {
+      host: text,
+      path: '/',
+    }
+    //note the ress. had to put that because it conflicts with res in router
+    var request = https.request(options, function (ress) {
+      var data = '';
+      ress.on('data', function (chunk) {
+        data += chunk;
+      });
+      ress.on('end', function () {
+        var textVersion = require("textversionjs");
+        var plainText = textVersion(data);
+        var phrases = data.split("\n");
+        console.log(phrases);
+        for(var i = 0; i < phrases.length; i++){
+          var result = libphonenumber.parse(phrases[i], "CA");
+          if (!isEmpty(result)) {
+              rt.push(libphonenumber.format(result, 'National'));
+          }
+        }
+        return res.status(200).send(rt);
+      });
+    });
+    request.on('error', function (err) {
+      console.log(err.message);
+    });
+    request.end();
+});
+
 // send file page
 router.get('/file', (req, res) => {
     res.sendFile(__dirname + "/fileupload.html");
@@ -44,11 +124,11 @@ router.post('/file', function(req, res) {
     } else {
         let mfile = req.files.mfile;
         var rt = [];
-        
+
         var fs = require('fs');
         fs.readFileSync(mfile.name).toString().split('\n').forEach(function (line) {
             var phone = libphonenumber.parse(line, "CA");
-            
+
             if (!isEmpty(phone)) {
                 rt.push(libphonenumber.format(phone, 'National'));
             }
